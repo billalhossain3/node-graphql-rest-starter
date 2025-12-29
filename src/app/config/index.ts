aefab4f40cb808261efe4d4c36cf7ac1670f05d8
billalhossain3 <billal@skygoalnext.com>
2025-12-29 22:36:50 +0600

    removed unnecessary configs

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -11,30 +11,12 @@ export default {
   // Database
   mongodb_url: process.env.DATABASE_URL,
 
-  // APIs
-  fred_api_key: process.env.FRED_API_KEY,
-  numbeo_api_key: process.env.NUMBEO_API_KEY,
-
   // Secret
   default_password: process.env.DEFAULT_PASS,
   super_admin_password: process.env.SUPER_ADMIN_PASSWORD,
   reset_pass_ui_link: process.env.RESET_PASS_UI_LINK,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   otp_bcrypt_salt_rounds: process.env.OTP_BCRYPT_SALT_ROUNDS,
-
-  // Stripe
-  stripe_secret_key: process.env.STRIPE_SECRET_KEY,
-  stripe_consultation_subscribe_webhook_secret:
-    process.env.STRIPE_CONSULTATION_SUBSCRIBE_WEBHOOK_SECRET,
-
-  stripe_secret_key_test: process.env.STRIPE_SECRET_KEY_TEST,
-  stripe_webhook_secret_key_test: process.env.STRIPE_WEBHOOK_SECRET_TEST,
-
-  // Zepto Mail
-  zepto_smpt_user: process.env.ZEPTO_SMTP_USER,
-  zepto_smpt_pass: process.env.ZEPTO_SMTP_PASS,
-  zepto_api_token: process.env.ZEPTO_API_TOKEN,
-  zepto_api_url: process.env.ZEPTO_API_URL,
 
   // JWT
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
